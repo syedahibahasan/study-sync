5c75c1d599bd93ce6a3306a9e476f7f4804089a9
venajeremy <venajeremy@gmail.com>
2024-12-10 16:47:15 -0800

    save

--- src/components/TimeSelector/TimeSelector.js
+++ src/components/TimeSelector/TimeSelector.js
@@ -218,7 +218,7 @@ const TimeSelector = ({
         console.log(updatedTimes)
         setSelectedTimes(updatedTimes);
         const updateTimes = [...formatedGroupTime];
-        updateTimes[(columnIndex*7)+rowIndex] = !updateTimes[(columnIndex*7)+rowIndex];
+        updateTimes[(columnIndex*27)+rowIndex] = !updateTimes[(columnIndex*27)+rowIndex];
         setFormatedGroupTime(updateTimes);
     };
 
